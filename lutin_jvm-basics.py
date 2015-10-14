39c244ff1a519587663668005e7ecb099c4445e3
Edouard DUPIN <yui.heero@gmail.com>
2015-10-14 21:21:03 +0200

    [DEV] update new lutin 0.8.0

--- lutin_jvm-basics.py
+++ lutin_jvm-basics.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python
 import lutin.module as module
 import lutin.tools as tools
+import os
+
+
+def get_type():
+	return "LIBRARY"
 
 def get_desc():
 	return "jvm basis call for all element who have jvm access"
 
-def create(target):
-	# module name is 'edn' and type binary.
-	my_module = module.Module(__file__, 'jvm-basics', 'LIBRARY')
+def get_licence():
+	return "APACHE-2"
+
+def get_compagny_type():
+	return "com"
+
+def get_compagny_name():
+	return "atria-soft"
+
+def get_maintainer():
+	return ["Mr DUPIN Edouard <yui.heero@gmail.com>"]
+
+def get_version():
+	return [1,0]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
 	my_module.add_module_depend(['etk'])
-	# add extra compilation flags :
 	my_module.add_extra_compile_flags()
-	# add the file to compile:
 	my_module.add_src_file([
 		'jvm-basics/debug.cpp',
 		'jvm-basics/jvm-basics.cpp'
@@ -19,7 +36,7 @@ def create(target):
 	my_module.add_header_file([
 		'jvm-basics/jvm-basics.h'
 		])
-	my_module.compile_version_XX(2011)
+	my_module.compile_version("c++", 2011)
 	my_module.add_path(tools.get_current_path(__file__))
 	return my_module
 
