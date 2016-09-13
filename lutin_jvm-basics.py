2d0262e8de6abc37aee2c777232d7f8226d07591
Edouard DUPIN <yui.heero@gmail.com>
2016-09-13 23:40:12 +0200

    [DEV] missing compilation lutin 2.0 update

--- lutin_jvm-basics.py
+++ lutin_jvm-basics.py
@@ -27,8 +27,8 @@ def get_version():
 
 def create(target, module_name):
 	my_module = module.Module(__file__, module_name, get_type())
-	my_module.add_module_depend(['etk'])
-	my_module.add_extra_compile_flags()
+	my_module.add_depend(['etk'])
+	my_module.add_extra_flags()
 	my_module.add_src_file([
 		'jvm-basics/debug.cpp',
 		'jvm-basics/jvm-basics.cpp'
